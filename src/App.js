c18e0b1acb0813fd32195c13f7d6466f37c59b66
AdemilsonSimiao <biassistencia@gmail.com>
2022-04-01 19:46:29 -0300

    SecondCommit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
+import './App.css';
 import MovieRows from './components/movieRows';
+import FeatureMovie from './components/featureMovie';
+import Header from './components/Header';
+
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default  () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState([null])
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(chosenInfo);
     };
     loadAll();
   }, []);
 
   return (
     <div className="page">
+      <Header />
+      {featureData &&
+        <FeatureMovie item={featureData} />
+      }
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRows key={key} title={item.title} items={item.items} />
--- src/components/movieRows.js
+++ src/components/movieRows.js
@@ -5,10 +5,14 @@ export default ({title, items}) => {
     return (
        <div>
            <h2>{title}</h2>
-           <div>
-                {items.results.length > 0 && items.results.map((item, key)=>(
-                    <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`} alt={item.original_title}/>
-                ))}
+           <div className="movieRow--lisarea">
+               <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key)=>(
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face${item.poster_path}`} alt={item.original_title}/>
+                        </div>
+                    ))}
+                </div>
            </div>
        </div>
     );
